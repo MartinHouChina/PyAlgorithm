5adbc039e66a78a4b60a4d2dd669165fb421316f
MartinHouChina <96623348+MartinHouChina@users.noreply.github.com>
2024-10-14 14:32:58 +0800

    Update Modulus.py

--- Modulus.py
+++ Modulus.py
@@ -1,11 +1,15 @@
 class Modulus:
     def __init__(self, num, mod):
-        self.num = num % mod
+        
+        if num < 0: num += mod
+        elif num >= mod: num -= mod
+        
+        self.num = num
         self.mod = mod
     
     def __add__(self, other):  
         if isinstance(other, int):  
-            return Modulus(self.num + other, self.mod)  
+            return Modulus(self.num + other % self.mod, self.mod)  
         elif isinstance(other, Modulus):  
             if other.mod != self.mod:  
                 raise ValueError
@@ -16,7 +20,7 @@ class Modulus:
         
     def __iadd__(self, other):
         if isinstance(other, int):  
-            self = Modulus(self.num + other, self.mod)  
+            self = Modulus(self.num + other % self.mod, self.mod)  
             return self
         elif isinstance(other, Modulus):  
             if other.mod != self.mod:  
@@ -29,48 +33,48 @@ class Modulus:
         
     def __mul__(self, other):  
         if isinstance(other, int):  
-            return Modulus(self.num * other, self.mod)
+            return Modulus(self.num * other % self.mod, self.mod)
         elif isinstance(other, Modulus):  
             if other.mod != self.mod:  
                 raise ValueError
             else:  
-                return Modulus(self.num * other.num, self.mod)   
+                return Modulus(self.num * other.num % self.mod, self.mod)   
         else:  
             raise TypeError
         
     def __imul__(self, other):  
         if isinstance(other, int):  
-            self = Modulus(self.num * other, self.mod)
+            self = Modulus(self.num * other % self.mod, self.mod)
             return self
         elif isinstance(other, Modulus):  
             if other.mod != self.mod:  
                 raise ValueError 
             else:  
-                self = Modulus(self.num * other.num, self.mod)   
+                self = Modulus(self.num * other.num % self.mod, self.mod)   
                 return self
         else:  
             raise TypeError
         
     def __sub__(self, other):  
         if isinstance(other, int):  
-            return Modulus(self.num - other % self.mod + self.mod, self.mod)
+            return Modulus(self.num - other % self.mod, self.mod)
         elif isinstance(other, Modulus):  
             if other.mod != self.mod:  
                 raise ValueError  
             else:  
-                return Modulus(self.num - other.num + self.mod, self.mod)
+                return Modulus(self.num - other.num, self.mod)
         else:  
             raise TypeError
         
     def __isub__(self, other):  
         if isinstance(other, int):  
-            self = Modulus(self.num - other % self.mod + self.mod, self.mod)
+            self = Modulus(self.num - other % self.mod, self.mod)
             return self
         elif isinstance(other, Modulus):  
             if other.mod != self.mod:  
                 raise ValueError
             else:  
-                return Modulus(self.num - other.num + self.mod, self.mod)
+                return Modulus(self.num - other.num, self.mod)
         else:  
             raise TypeError
         
@@ -98,12 +102,12 @@ class Modulus:
     
     def __floordiv__(self, other):
         if isinstance(other, int):  
-            return Modulus(self.num * pow(other, self.mod - 2, self.mod), self.mod)
+            return Modulus(self.num * pow(other, self.mod - 2, self.mod) % self.mod, self.mod)
         elif isinstance(other, Modulus):  
             if other.mod != self.mod:  
                 raise ValueError  
             else:  
-                return Modulus(self.num * pow(other.num, self.mod - 2, self.mod), self.mod)
+                return Modulus(self.num * pow(other.num, self.mod - 2, self.mod) % self.mod, self.mod)
         else:  
             raise TypeError
     
@@ -112,7 +116,7 @@ class Modulus:
 
     def __ifloordiv__(self, other):
         if isinstance(other, int):  
-            self = Modulus(self.num * pow(other, self.mod - 2, self.mod), self.mod)
+            self = Modulus(self.num * pow(other, self.mod - 2, self.mod) % self.mod, self.mod)
             return self
         elif isinstance(other, Modulus):  
             if other.mod != self.mod:  
@@ -133,7 +137,7 @@ class Modulus:
         return self * other
 
     def __neg__(self):
-        return Modulus(self.mod - self.num if self.num != 0 else 0, self.mod)
+        return Modulus(-self.num, self.mod)
 
     def __rsub__(self, other):
         return -self + other
